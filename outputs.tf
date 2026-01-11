f758ee84d04b85800375740c80404679ccb0566a
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:04:57 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "sentinel_data_connector_azure_security_centers" {
+  description = "All sentinel_data_connector_azure_security_center resources"
+  value       = azurerm_sentinel_data_connector_azure_security_center.sentinel_data_connector_azure_security_centers
+}
+
