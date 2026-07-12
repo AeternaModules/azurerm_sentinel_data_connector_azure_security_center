7c47933dddb70df997be81d48655e87b3ff8c5bf
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:13 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "sentinel_data_connector_azure_security_centers" {
-  description = "All sentinel_data_connector_azure_security_center resources"
-  value       = azurerm_sentinel_data_connector_azure_security_center.sentinel_data_connector_azure_security_centers
-}
 output "sentinel_data_connector_azure_security_centers_log_analytics_workspace_id" {
-  description = "List of log_analytics_workspace_id values across all sentinel_data_connector_azure_security_centers"
-  value       = [for k, v in azurerm_sentinel_data_connector_azure_security_center.sentinel_data_connector_azure_security_centers : v.log_analytics_workspace_id]
+  description = "Map of log_analytics_workspace_id values across all sentinel_data_connector_azure_security_centers, keyed the same as var.sentinel_data_connector_azure_security_centers"
+  value       = { for k, v in azurerm_sentinel_data_connector_azure_security_center.sentinel_data_connector_azure_security_centers : k => v.log_analytics_workspace_id }
 }
 output "sentinel_data_connector_azure_security_centers_name" {
-  description = "List of name values across all sentinel_data_connector_azure_security_centers"
-  value       = [for k, v in azurerm_sentinel_data_connector_azure_security_center.sentinel_data_connector_azure_security_centers : v.name]
+  description = "Map of name values across all sentinel_data_connector_azure_security_centers, keyed the same as var.sentinel_data_connector_azure_security_centers"
+  value       = { for k, v in azurerm_sentinel_data_connector_azure_security_center.sentinel_data_connector_azure_security_centers : k => v.name }
 }
 output "sentinel_data_connector_azure_security_centers_subscription_id" {
-  description = "List of subscription_id values across all sentinel_data_connector_azure_security_centers"
-  value       = [for k, v in azurerm_sentinel_data_connector_azure_security_center.sentinel_data_connector_azure_security_centers : v.subscription_id]
+  description = "Map of subscription_id values across all sentinel_data_connector_azure_security_centers, keyed the same as var.sentinel_data_connector_azure_security_centers"
+  value       = { for k, v in azurerm_sentinel_data_connector_azure_security_center.sentinel_data_connector_azure_security_centers : k => v.subscription_id }
 }
 
